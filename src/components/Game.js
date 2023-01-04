58fc34cec6ac44650449d029ac0e62bd95eceef5
robson <robson.jesus@unidavi.edu.br>
2023-01-04 17:32:18 -0300

    estruturando o componente do jogo

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,30 @@ import "./Game.css";
 
 const Game = ({ verifyletter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyletter}>Finalizar  jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palvra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra : <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blacnkSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="latter" maxLength="1" required />
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   );
 };
