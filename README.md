d2f84e6d4f97ca0e5fd669ca0e28de8101045539
robson <robson.jesus@unidavi.edu.br>
2023-01-04 16:33:49 -0300

    Criação de todos os componentes do projeto e hospedagem da aplicação na vercel.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Getting Started with Create React App
 
+
+
+Link do projeto online[LINK:](https://secret-word-rho.vercel.app/).
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+//CSS
 import './App.css';
+
+
+//React
+import { useCallback, useEffect, useState } from 'react';
+
+
+// Importação dos dados 
+import { wordsList }  from './data/words';
+
+// Importação de componentes
 import StartScren from './components/StartScren';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+]
 
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  console.log(words)
+
   return (
     <div className="App">
-      <StartScren/>
+      {gameStage === 'start' && <StartScren/>}
+      {gameStage === 'game' && <Game/>}
+      {gameStage === 'end' && <GameOver/>}
     </div>
   );
 }
