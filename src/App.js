cd21c32089f306b5ecb3a8bc64cde6da5353e6b3
robson <robson.jesus@unidavi.edu.br>
2023-01-04 18:40:12 -0300

    Finalização tela de fim do jogo

--- src/App.js
+++ src/App.js
@@ -139,7 +139,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === "end" && <GameOver retry={retry} />}
+      {gameStage === "end" && <GameOver retry={retry} score={score}/>}
     </div>
   );
 }
